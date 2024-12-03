ee50df8d794a97b1b306968eebd31eae6355dce9
douglas <douglasmendes97511@gmail.com>
2024-12-03 19:11:33 -0300

    alteracoes vinicius

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContentService } from '../services/content.service'; // Certifique-se de que o caminho está correto
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  content: any = {}
 
+  constructor(private contentService: ContentService) { }
+
+  ngOnInit(): void {
+    // Se inscreve no conteúdo compartilhado pelo serviço
+    this.contentService.content$.subscribe((data) => {
+      this.content = data.footer ||{}; // Acessa o conteúdo específico para a página 'home'
+      });
+  }
 }
