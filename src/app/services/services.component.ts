4fbc7e1bee5d73b1aea24193c353ae3c68085d90
vNemetz <vininemetz@gmail.com>
2025-03-07 11:49:26 -0300

    Componentizando serviços

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { templateTextService } from '../services/templateText.service';
+
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrl: './services.component.scss'
+  styleUrls: ['./services.component.scss']
 })
-export class ServicesComponent {
+export class ServicesComponent implements OnInit{
+  templateText: any = {};
 
+  constructor(private templateTextService: templateTextService) {}
+
+  ngOnInit() {
+    this.templateTextService.templateText$.subscribe(data => {
+      this.templateText = data.services;
+    });
+  }
 }
+
